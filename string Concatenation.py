391ec827352690e7e6e103170a800a42e52bbea9
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-11 00:20:12 +0530

    Add files via upload

---
+++ string Concatenation.py
@@ -0,0 +1,35 @@
+# a="Hello"
+# b="vidhi"
+# c= a + " "+b
+# print(c)
+
+# age=36
+# txt="My name is John,I am {} "
+# print(txt.format(age))
+
+
+# quantity=3
+# itemno=543
+# price=453
+
+# myorder="I want {} piece of item{} for {} dollars."
+# print(myorder.format(quantity,itemno,price))
+
+#boolean
+# print(10>9)
+# print(10==9)
+# print(10<9)
+
+# a=200
+# b=5
+
+# if b>a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+print(bool("hello"))
+print(bool(13))
+    
+    
+
